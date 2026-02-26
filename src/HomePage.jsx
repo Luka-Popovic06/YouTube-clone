588ec559c38349758af205edb68a8b93b0cb64d3
Luka Popovic <lukapopovic0111@gmail.com>
2026-02-26 18:01:17 +0100

    add Home component with videos rendering

---
+++ src/HomePage.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import userContext from "./UserContext";
+import { NavLink } from "react-router-dom";
+const Home = () => {
+  const { videos, timeAgo } = useContext(userContext);
+
+  return (
+    <>
+      <h1>
+        New <span className="category-name">videos</span>
+      </h1>
+      <div className="link-container">
+        {videos &&
+          videos?.map((obj, index) => (
+            <NavLink className="video-card" key={index}>
+              <img
+                className="video-image"
+                src={obj.snippet.thumbnails.high.url}
+                alt="vid"
+              />
+              <div className="video-info-box">
+                <h5 className="heading">{obj.snippet.title}</h5>
+                <p className="chanel-name">{obj.snippet.channelTitle}</p>
+                <p className="date">{timeAgo(obj.snippet.publishTime)}</p>
+              </div>
+            </NavLink>
+          ))}
+      </div>
+    </>
+  );
+};
+export default Home;
