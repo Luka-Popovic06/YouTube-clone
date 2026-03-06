82c2c7afcc5bef9745f63e54dd761193fa2dc869
Luka Popovic <lukapopovic0111@gmail.com>
2026-03-06 10:52:44 +0100

    feat: add loadChannel function to fetch channel data

--- src/api/youtubeApi.js
+++ src/api/youtubeApi.js
@@ -27,3 +27,28 @@ export const loadRelatedVideos = async (videoCategory) => {
     return [];
   }
 };
+export const loadChannel = async (channelName) => {
+  const options = {
+    method: "GET",
+    url: "https://youtube-v31.p.rapidapi.com/search",
+    params: {
+      q: channelName,
+      part: "snippet,id",
+      type: "channel",
+      order: "date",
+      maxResults: "1",
+    },
+    headers: {
+      "x-rapidapi-key": import.meta.env.VITE_RAPIDAPI_KEY,
+      "x-rapidapi-host": "youtube-v31.p.rapidapi.com",
+      "Content-Type": "application/json",
+    },
+  };
+  try {
+    const response = await axios.request(options);
+    return response.data.items;
+  } catch (error) {
+    console.error("Greška pri učitavanju videa:", error);
+    return [];
+  }
+};
