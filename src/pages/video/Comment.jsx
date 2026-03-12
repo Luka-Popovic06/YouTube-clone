b8764875584fe9c85a8b3284101a8d33c32586b5
Luka Popovic <lukapopovic0111@gmail.com>
2026-03-12 13:51:32 +0100

    Add Comment component

--- src/pages/video/Comment.jsx
+++ src/pages/video/Comment.jsx
@@ -1,1 +1,24 @@
-
+import { timeAgo } from "../../utils/formatData";
+const Comment = ({ obj }) => {
+  return (
+    <div className="comment">
+      <img
+        src={obj?.snippet.topLevelComment.snippet.authorProfileImageUrl}
+        alt="author-img"
+        className="comment-img"
+      />
+      <div className="comment-info">
+        <p className="comment-owner-name">
+          {obj?.snippet.topLevelComment.snippet.authorDisplayName}
+        </p>
+        <p className="time">
+          {timeAgo(obj?.snippet.topLevelComment.snippet.publishedAt)}
+        </p>
+      </div>
+      <p className="comment-text">
+        {obj?.snippet.topLevelComment.snippet.textDisplay}
+      </p>
+    </div>
+  );
+};
+export default Comment;
